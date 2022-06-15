73be675babf6588244ce791f025ba031319d3bd7
Julio Angulo <solojoas@gmail.com>
2022-06-15 01:10:16 +0000

    Configured boost::tests to use boost::test framework

--- components/table/tests/tests.cpp
+++ components/table/tests/tests.cpp
@@ -1,10 +1,19 @@
-#include <iostream>
-#include "test_table.h"
+// #include <iostream>
+// #include "test_table.h"
 
-int main()
-{
-    std::cout << "Running tests..." << std::endl;
-    test_table();
+// int main()
+// {
+//     std::cout << "Running tests..." << std::endl;
+//     test_table();
    
-    return 0;
+//     return 0;
+// }
+#define BOOST_TEST_MODULE My Test 
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(first_test)
+{
+  int i = 1;
+  BOOST_TEST(i);
+  BOOST_TEST(i == 1);
 }
